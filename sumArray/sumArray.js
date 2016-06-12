380eb0fb5b0a3abe3a4e982e321ddfc740f9f44a
pavelmp <pavelmp@gmail.com>
2016-06-12 18:22:46 +0300

    Improve description of the exercise

--- sumArray/sumArray.js
+++ sumArray/sumArray.js
@@ -1,7 +1,10 @@
 /*
- * Given an array of numbers, calculate the greatest contiguous sum of numbers in it.
+ * Given an array of numbers, calculate the greatest continuous sum of numbers in it. 
+ * Continuous sum means sum of numbers that are next to each other
  * A single array item will count as a contiguous sum.
- *
+ * Example: 
+ * var arr = [1,2,3,-3,2];
+ * sumArray(arr);//should return 6 because the greatest continuous sum is 1+2+3. If we include -3 and 2, the sum will be smaller.
  */
 
 var sumArray = function(array) {
