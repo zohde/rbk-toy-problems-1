3f786167dd20cfb949e754fa2334259b6c72280f
Admin <admin@NetBoot-2.local>
2016-06-13 12:31:33 +0400

    first toy done

--- isSubsetOf/isSubsetOf.js
+++ isSubsetOf/isSubsetOf.js
@@ -1,4 +1,5 @@
-/* * Make an array method that can return whether or not a context array is a
+/*
+ * Make an array method that can return whether or not a context array is a
  * subset of an input array.  To simplify the problem, you can assume that neither
  * array will contain objects or arrays as elements within them.
  *
@@ -17,7 +18,29 @@
 */
 
 
-/* * Extra credit: Make the method work for arrays that contain objects and/or arrays as elements.*/
+/*
+ * Extra credit: Make the method work for arrays that contain objects and/or arrays as elements.
+*/
 
 Array.prototype.isSubsetOf = function (arr) {
-}
+	//first solution 
+	//var subString=arr.join("");
+	//var mainString=this.join("");
+	//console.log(mainString.indexOf(subString) > -1);
+	// it is wroooooong
+
+	//secand solution
+	var bigArray=arr;
+	var isAll=0;
+	for (var i = 0; i < bigArray.length; i++) {
+		for (var j = 0; j < this.length; j++) {
+			if(this[j] === bigArray[i]){
+				isAll++;
+			}
+		}
+	}
+	if(isAll === this.length){
+		return true
+	}
+	return false;
+}
