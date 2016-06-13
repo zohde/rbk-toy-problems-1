0339ee6a14d82cd54f9357e06189a85189ef74cf
Bader Khalifeh <bader.a.khalifeh@gmail.com>
2016-06-13 11:39:48 +0300

    Solved the toy problem and added some test cases

--- isSubsetOf/spec/isSubsetOf.js
+++ isSubsetOf/spec/isSubsetOf.js
@@ -1,5 +1,19 @@
 describe('isSubsetOf', function() {
   it('should return the right answer', function() {
+  	var a = ['commit','push'];
+  	var b = ['merge','reset','reset'];
     expect(1).to.equal(1);
+    expect(a.isSubsetOf(['commit','rebase','push','blame'])).to.equal(true);
+    expect(b.isSubsetOf(['reset','merge','add','commit'])).to.equal(true);
+  });
+  it('should work on array containing sub-Arrays', function() {
+  	var a = [[1,2,3],[4,5],6]
+    expect(a.isSubsetOf([[1],[6],1,2,3,[1,2,3],4,5,[4,5]])).to.equal(false);
+    expect(a.isSubsetOf([[1],[6],1,2,3,[1,2,3],4,5,[4,5],6])).to.equal(true);
+  });
+  it('should work on array containing objects', function() {
+  	var a = [{'a': 2, 'b': [1,2,3]},{'c':{d:2}}];
+  	expect(a.isSubsetOf([[1,2,3],[{'a': 2, 'b': [1,2,3]},{'c':{d:2}}]])).to.equal(false);
+    expect(a.isSubsetOf([[1,2,3],{'a': 2, 'b': [1,2,3]},{'c':{d:2}}])).to.equal(true);
   });
 });
