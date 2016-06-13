52bd18830572723b50fc43227095a0863cf13960
AwsAhmed <aws.y.ahmed@gmail.com>
2016-06-13 12:02:03 +0300

    Aws try

--- isSubsetOf/isSubsetOf.js
+++ isSubsetOf/isSubsetOf.js
@@ -1,4 +1,5 @@
-/* * Make an array method that can return whether or not a context array is a
+/*
+ * Make an array method that can return whether or not a context array is a
  * subset of an input array.  To simplify the problem, you can assume that neither
  * array will contain objects or arrays as elements within them.
  *
@@ -17,7 +18,27 @@
 */
 
 
-/* * Extra credit: Make the method work for arrays that contain objects and/or arrays as elements.*/
+/*
+ * Extra credit: Make the method work for arrays that contain objects and/or arrays as elements.
+*/
 
 Array.prototype.isSubsetOf = function (arr) {
-}
+	var flag = 0;
+	for (var i = 0; i < this.length; i++) {
+		for (var j = 0; j < arr.length; j++) {
+			if (arr[j] === this[i]){
+				flag++;
+			}
+		}
+	}
+if (flag === arr.length){
+	return true;
+} else return false;
+}
+
+var MyArray = new Array;
+MyArray[0] = "a";
+MyArray[1] = "b";
+MyArray[2] = "c";
+
+MyArray.isSubsetOf(["a","b","c","d"]);
