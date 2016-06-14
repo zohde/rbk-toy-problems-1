db018bdff1bd3679aaaa7d6f72961cefecd30636
AwsAhmed <aws.y.ahmed@gmail.com>
2016-06-14 12:02:23 +0300

    trying.....

--- rockPaperScissors/rockPaperScissors.js
+++ rockPaperScissors/rockPaperScissors.js
@@ -17,6 +17,36 @@
 *
 */
 
+
+
+
+// ================================================================================
+// each element of the types going first element then the second two types.
+// rock with others is three 
+
+// first [[1,1,1],[2,2,2], [3,3,3],  ]
+
+
 var rockPaperScissors = function() {
-  // fill me out!
+  var games = 3;
+  var types1 = ["rock", "paper", "scissors"];
+  var types2 = ["rock", "paper", "scissors"];
+  var types3 = ["rock", "paper", "scissors"];
+  var singlePosibility = [];
+  var allPossibilities = [];
+
+  //var theBigArrayDimention  = 729;
+  for (var i = 0; i < types1.length; i++) {
+  	for (var j = 0; j<types2.length; j++){
+  		for (var i = 0; var k < types3.length; i++) {
+  		singlePosibility.push(types1[i]);
+  		singlePosibility.push(types2[j]);
+  		singlePosibility.push(types3[k]);
+  	}
+  	console.log(singlePosibility);
+  	allPossibilities.push(singlePosibility);
+//  	  	singlePosibility = [];
+  }
+}
+return allPossibilities;
 };
