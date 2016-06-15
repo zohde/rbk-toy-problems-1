63eace3982158150bc2057dba8ecac246bf0f2c4
J <jeongjueun@gmail.com>
2016-06-15 15:47:25 +0300

    testing for merge conflict

--- functionBind/functionBind.js
+++ functionBind/functionBind.js
@@ -1,21 +1,21 @@
 
- // function bind():
- //  example 1:
- //  var alice = {
- //   name: 'alice',
- //   shout: function(){
- //     alert(this.name);
- //   }
- // }
- // var boundShout = bind(alice.shout, alice);
- // boundShout(); // alerts 'alice'
- // boundShout = bind(alice.shout, {name: 'bob'});
- // boundShout(); // alerts 'bob'
- //  example 2:
- //  var func = function(a, b){ return a + b };
- // var boundFunc = bind(func, null, 'foo');
- // var result = boundFunc('bar');
- // result === 'foobar'; // true
+ function bind():
+  example 1:
+  var alice = {
+   name: 'alice',
+   shout: function(){
+     alert(this.name);
+   }
+ }
+ var boundShout = bind(alice.shout, alice);
+ boundShout(); // alerts 'alice'
+ boundShout = bind(alice.shout, {name: 'bob'});
+ boundShout(); // alerts 'bob'
+  example 2:
+  var func = function(a, b){ return a + b };
+ var boundFunc = bind(func, null, 'foo');
+ var result = boundFunc('bar');
+ result === 'foobar'; // true
 
 
 var bind = function(func, context) {
