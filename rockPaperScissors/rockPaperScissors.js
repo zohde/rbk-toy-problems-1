5f8a62b380a1fbdb1d05f49d7e2a65d1650498b6
Saif Elokour <saiffelokour@gmail.com>
2016-06-16 13:27:11 +0300

    Added Extra Credit

--- rockPaperScissors/rockPaperScissors.js
+++ rockPaperScissors/rockPaperScissors.js
@@ -17,16 +17,41 @@
 *
 */
 
+Array.prototype.isSubsetOf = function (arr) {
+	var bool = 0
+	
+	for (var i = 0; i < arr.length; i++) {
+		for(var j = 0; j < this.length; j++){
+			if(JSON.stringify(this[j]) === JSON.stringify(arr[i])){
+				bool++
+			}
+		}
+	}
+	return bool >= this.length ? true : false 
+}
+
+
 var rockPaperScissors = function(n) {
-  n = 3
-  var rpc = ['rock', 'paper', 'scissors']
-  var result = []
-  for(var i = 0; i < n; i++){
-  	 for(var j = 0; j < n; j++){
-  		for(var k = 0; k < n; k++){
-  			result.push([rpc[i], rpc[j], rpc[k]])
-  		}
-  	 }
-  }
-  return result
-};
+	var result = []
+	var rpc = ['rock', 'paper', 'scissors']
+	var numSol = Math.pow(3, n)
+	while(result.length < numSol){
+		var innerArr = []
+		for(var i = 0; i < n; i++){
+			var rnd = Math.floor(Math.random() * 3)
+			if(rnd === 0){
+				innerArr.push(rpc[0])
+			} else if (rnd === 1){
+				innerArr.push(rpc[1])
+			} else if (rnd === 2){
+				innerArr.push(rpc[2])
+			}
+		}
+
+		if(!innerArr.isSubsetOf(result)){
+			result.push(innerArr)
+		}
+	}
+	return result
+}
+
