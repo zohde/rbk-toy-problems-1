505d673269b41702d86276b226bb67bd88d94430
your name <ckmohammed89@gmail.com>
2016-06-20 11:44:41 +0300

    finish the problem

--- balancedParens/balancedParens.js
+++ balancedParens/balancedParens.js
@@ -1,4 +1,4 @@
-/*
+/* 
  * write a function that takes a string of text and returns true if
  * the parentheses are balanced and false otherwise.
  *
@@ -24,6 +24,28 @@
  *
  */
 
- var balancedParens = function (input) {
-   // write your code hre
- };
+var balancedParens = function (string) {
+   
+  var leftSide=[];
+   var rightSide=[];
+   for (var i=0; i<string.length; i++) {
+
+    	if(string[i]==='(' || string[i]==='{' || string[i]==='['){
+   		 leftSide.push(string[i]);  	
+     	}else if (string[i]===')' || string[i]==='}' || string[i]===']'){
+   	              rightSide.push(string[i])
+        }
+   }
+   for (var i=0; i<leftSide.length; i++) {
+   	 for (var j=rightSide.length-1; j>=0; j--) {
+   		if(leftSide.length !== rightSide.length){
+   			return false;
+   		}
+   		else if((rightSide[j]==='}' && leftSide[i]==='{') ||
+   			    (rightSide[j]===')' && leftSide[i]==='(') || 
+   			    (rightSide[j]===']' && leftSide[i]==='['))
+   			return true;
+   		return false;
+     }
+   }
+};
