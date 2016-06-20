43daec30c7c0556689de5b7b23052655e312db8d
Mohannad Bakkar <mohannadbakkar@gmail.com>
2016-06-20 11:41:46 +0300

    Finshed step one and two

--- balancedParens/balancedParens.js
+++ balancedParens/balancedParens.js
@@ -25,5 +25,33 @@
  */
 
  var balancedParens = function (input) {
-   // write your code hre
+ 	var counterfor1 =0;
+	var counterfor2 = 0;
+	var counterfor3 =0;
+	var counterfor4 =0;
+	var counterfor5 =0;
+	var counterfor6 =0;
+ 	if(input.length%2===1){
+ 		return false;
+ 	}
+  for (var i = 0; i < input.length; i++) {
+  	if(input[i]==='{'){
+  		counterfor3++
+  	}else if(input[i]==='}'){
+  		counterfor4++
+  	}else if(input[i]==='['){
+  		counterfor5++
+  	}else if(input[i]===']'){
+  		counterfor6++
+  	}
+  	else if(input[i]==='('){
+  		counterfor1++
+  	  }else if(input[i]===')'){
+  	  		counterfor2++
+  	  }
+  	}
+  	if(counterfor1===counterfor2||counterfor3===counterfor4||counterfor5===counterfor4){
+  		return true;
+  	}
+  	return false;
  };
