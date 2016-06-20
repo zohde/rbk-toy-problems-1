dfc5ca9ddb5573b4e711ccaf62ab1e705e9f654a
housamaldeen <housam993@gmail.com>
2016-06-20 12:00:21 +0300

    Implement balancedParens function

--- balancedParens/balancedParens.js
+++ balancedParens/balancedParens.js
@@ -25,5 +25,59 @@
  */
 
  var balancedParens = function (input) {
-   // write your code hre
+ 	var s = input.split('');
+    for (var i = 0; i < s.length; i++) {
+   		if(s[i] === '('){
+   			var flag = true;
+   			//debugger;
+   			for (var j = s.length - 1 ; j >= 0 && j > i; j--) {
+   				if(s[j] === ')'){
+   					s.splice(j,1);
+   					s.splice(i,1);
+    				flag = false;
+    				i--;
+    				j = 0;
+   				}
+   			}
+   			if(flag){
+   				return false;
+   			}
+   		}
+   }
+   if(s.length === 0){
+   	return true;
+   }
+   else{
+   	return false;
+   }
+ };
+var balancedParens = function (input) {
+ 	var s = input.split('');
+ 	var open = ['(','{','[']
+ 	var close = [')','}',']']
+    for (var i = 0; i < s.length; i++) {
+   		if(open.indexOf(s[i]) !== -1){
+   			var flag = true;
+   			var index = open.indexOf(s[i])
+   			//debugger;
+   			for (var j = s.length - 1 ; j >= 0 && j > i; j--) {
+   				if(s[j] === close[index]){
+   					s.splice(j,1);
+   					s.splice(i,1);
+    				flag = false;
+    				i--;
+    				j = 0;
+   				}
+   			}
+   			if(flag){
+   				return false;
+   			}
+   		}
+   }
+   
+   for (var i = 0; i < open.length; i++) {
+   	if(s.indexOf(open[i]) === -1 && s.indexOf(close[i]) === -1)
+   		return true;
+   }
+   return false;
  };
