6fe4c04bab8488b797e1cf89accdc6794ca56e2a
AwsAhmed <aws.y.ahmed@gmail.com>
2016-06-21 11:55:47 +0300

    solution for commonCharecters

--- commonCharacters/commonCharacters.js
+++ commonCharacters/commonCharacters.js
@@ -11,6 +11,59 @@
  */
 
 //Works for any number of input strings:
+
+
 var commonCharacters = function(string1, string2) {
-  
+  var arr1 = string1.split("");
+  var arr2 = string2.split("");
+  var result = "";
+  for (var i = 0; i < arr1.length; i++) {
+  	for (var j = 0; j < arr2.length; j++) {
+  		if (arr1[i] === arr2[j]) {
+  			if (result.indexOf(arr1[i])===-1){
+  				result+=arr1[i];	
+  			}
+  		}
+  	}
+  }
+  return result;
 };
+
+
+
+
+/*
+// create array of arguments.
+// make a for loop inside the arguments array
+// use a recursion for the first element in the arguments array for the following elements
+
+
+
+var commonCharacters = function() {
+  // console.log(arguments.length);
+  // console.log(arguments[0]);
+  var argumentsArray = [];
+  for (var i = 0; i < arguments.length; i++) {
+  	argumentsArray.push(arguments[i]);
+  }
+  //console.log(argumentsArray);
+  var arr1 = string1.split("");
+  var arr2 = string2.split("");
+  var result = "";
+  for (var i = 0; i < argumentsArray.length; i++) {
+  	argumentsArray[i]
+  
+  }
+  for (var i = 0; i < arr1.length; i++) {
+  	for (var j = 0; j < arr2.length; j++) {
+  		if (arr1[i] === arr2[j]) {
+  			if (result.indexOf(arr1[i])===-1){
+  				result+=arr1[i];	
+  			}
+  		}
+  	}
+  }
+  return result;
+};
+
+*/
