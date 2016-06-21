e2d65965e43052a4d0afe3c043a0bb8df6053d7f
Bader Khalifeh <bader.a.khalifeh@gmail.com>
2016-06-21 11:56:24 +0300

    Finished Basic Requirement

--- commonCharacters/commonCharacters.js
+++ commonCharacters/commonCharacters.js
@@ -4,13 +4,29 @@
  * order that they appeared in `str1`. Remember to skip spaces and characters you
  * have already encountered!
  *
- * Example: commonCharacters('acexivou', 'aegihobu')
+ * Example: commonCharacters('aceixvou', 'aegihobu')
  * Returns: 'aeiou'
  *
  * Extra credit: Extend your function to handle more than two input strings.
  */
 
 //Works for any number of input strings:
+var recursiveCommChars = function(result, str1, str2){	
+	if(str1.length === 0){
+		return result;
+	}
+	if(str2.indexOf(str1[0]) !== -1 ){
+		result += str1[0];
+		for(var i=1; i<str1.length; i++){
+			if(str1[i] === str1[0]){
+				str1.splice(i,1);
+			}
+		}
+	}
+	str1.splice(0,1);
+	return recursiveCommChars(result, str1, str2);
+}
+
 var commonCharacters = function(string1, string2) {
-  
+	return recursiveCommChars('', string1.split(''), string2.split(''));
 };
