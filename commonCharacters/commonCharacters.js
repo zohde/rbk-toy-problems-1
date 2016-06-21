1b85fd589b1e6698fd2e868e0be2ea603079665a
WeaamShahwan <shahwamweaam@gmail.com>
2016-06-21 11:48:08 +0300

    toyproblem commonCharacters

--- commonCharacters/commonCharacters.js
+++ commonCharacters/commonCharacters.js
@@ -12,5 +12,14 @@
 
 //Works for any number of input strings:
 var commonCharacters = function(string1, string2) {
-  
+  var commonChars ='';
+  for(var i=0; i<string1.length;i++){
+  	for(var j=0;j<string2.length;j++){
+  		if(string1[i]===string2[j]&&(commonChars.indexOf(string2[j])===-1)){
+  			commonChars+=string2[j]
+  		}
+
+  	}
+  }
+  return commonChars;
 };
