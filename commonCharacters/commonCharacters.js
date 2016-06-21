72d3b68dbde4f8104158957ba25fc699acfec707
rwaida <rwaida4@gmail.com>
2016-06-21 11:37:15 +0300

    best toy

--- commonCharacters/commonCharacters.js
+++ commonCharacters/commonCharacters.js
@@ -6,11 +6,43 @@
  *
  * Example: commonCharacters('acexivou', 'aegihobu')
  * Returns: 'aeiou'
+
  *
  * Extra credit: Extend your function to handle more than two input strings.
  */
 
 //Works for any number of input strings:
+//for may string...
+var commonCharacters = function(string1, string2){
+	var array=[];
+	var result=[];
+	var lastresult="";
+	for(var i=0;i<arguments.length;i++){
+		var arr=arguments[i];
+		array.push(arr);
+	}
+	for(var j=0;j<array.length-1;j++){
+		for(var k=0;k<array[j].length;k++){
+				if(array[j].indexOf(array[j+1][k])!==-1){
+					result.push(array[j+1][k]);
+			}
+		}
+	}
+	lastresult=result.join("");
+	return lastresult;
+} ;
+//for just 2 string...
 var commonCharacters = function(string1, string2) {
-  
+	var array1=string1.split("");
+	var array2=string2.split("");
+	var arr=[];
+	var str="";
+	for(var i=0;i<array1.length;i++){
+		if(array2.indexOf(array1[i])!==-1){
+			arr.push(array1[i])
+		}
+
+	}
+	str=arr.join("");
+	return str;
 };
