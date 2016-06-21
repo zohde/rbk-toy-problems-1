45eb23ad0a386325d642856e0c8e72545d034de7
Yan Fan <yan.fan@ayasdi.com>
2016-06-21 12:42:10 -0700

    balancedParens solution

--- balancedParens/balancedParens.js
+++ balancedParens/balancedParens.js
@@ -25,5 +25,33 @@
  */
 
  var balancedParens = function (input) {
-   // write your code hre
+   /* START SOLUTION */
+   // stack is an array that will help us keep track of all of our pairs
+   var stack = [];
+   var pairs = {
+     '{':'}',
+     '[':']',
+     '(':')'
+   };
+
+   // we want to loop through each character in the string input
+   for (var i = 0; i < input.length; i++){
+     // let's set the character we are currently on to a variable to make things cleaner
+     var chr = input[i];
+
+     // if we find a character that belongs in a pair, we want to push it to our 'stack' array
+     if (pairs[chr]){
+       stack.push(chr);
+    // else, we chaeck to see if our character is a closing bracket
+     } else if (chr === '}' || chr === ']' || chr === ')'){
+       // if it is, we want to check if it's matching!
+       if (pairs[stack.pop()] !== chr){
+         return false;
+       }
+     }
+   }
+
+   //return false if there are any unclosed brackets
+   return stack.length === 0;
+   /* END SOLUTION */
  };
