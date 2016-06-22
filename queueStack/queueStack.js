4faaf2003c23f69e83fbbf952cc1c59924eb44b5
Admin <admin@NetBoot.local>
2016-06-22 11:58:40 +0300

    updated

--- queueStack/queueStack.js
+++ queueStack/queueStack.js
@@ -7,17 +7,28 @@
    * Stack Class
    */
  var Stack = function() {
+  var pointer = 0 ; 
+  var storage=[] ; 
+ 
 
    // add an item to the top of the stack
-   this.push = function(){
+   this.push = function(value){
+   storage.push[value];
+    pointer++
+
    };
 
    // remove an item from the top of the stack
    this.pop = function(){
+    storage.pop() ;
+    pointer-- ; 
+
+
    };
 
    // return the number of items in the stack
    this.size = function(){
+    return storage.length ; 
    };
  };
 
@@ -25,22 +36,38 @@
    * Queue Class
    */
  var Queue = function() {
+ 
+
    // Use two `stack` instances to implement your `queue` Class
    var inbox = new Stack();
    var outbox = new Stack();
 
    // called to add an item to the `queue`
-   this.enqueue = function(){
+   this.enqueue = function(value){
+   
+   inbox.push(value); 
+
      // TODO: implement `enqueue`
    };
 
    // called to remove an item from the `queue`
    this.dequeue = function(){
+     if(this.size !== 0 ) { 
+      // if(inbox.size !== 0) { 
+       outbox.push(inbox.pop()) 
+      }
+
+
+
+
      // TODO: implement `dequeue`
+    }
+    return outbox.pop() ;
    };
 
    // should return the number of items in the queue
    this.size = function(){
+    return this.length ;
      // TODO: implement `size`
    };
  };
