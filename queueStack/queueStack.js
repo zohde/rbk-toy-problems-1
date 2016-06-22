41ec42348e8deb9c1e51856d35486b5fb56da171
Ola-Alkhateeb <ola.93.alkhateeb@gmail.com>
2016-06-22 11:54:04 +0300

    toy 5

--- queueStack/queueStack.js
+++ queueStack/queueStack.js
@@ -7,17 +7,20 @@
    * Stack Class
    */
  var Stack = function() {
-
+ 	this.storage = [] ;
    // add an item to the top of the stack
-   this.push = function(){
+   this.push = function(item){
+   	this.storage.unshift(item) ;
    };
 
    // remove an item from the top of the stack
    this.pop = function(){
+   	this.storage.shift() ;
    };
 
    // return the number of items in the stack
    this.size = function(){
+   	return this.storage.length ;
    };
  };
 
@@ -26,21 +29,27 @@
    */
  var Queue = function() {
    // Use two `stack` instances to implement your `queue` Class
-   var inbox = new Stack();
-   var outbox = new Stack();
+   var inbox = new Stack(); 
+   var outbox = new Stack(); 
 
    // called to add an item to the `queue`
-   this.enqueue = function(){
-     // TODO: implement `enqueue`
+   this.enqueue = function(item){
+    inbox.push(item); // TODO: implement `enqueue`
    };
 
    // called to remove an item from the `queue`
-   this.dequeue = function(){
-     // TODO: implement `dequeue`
-   };
+   this.dequeue = function(){ // if inbox stack is not empty , we want to remove all these values to the outbox stack 
+            while (inbox.size()!== 0) {
+               outbox.push(inbox.pop());
+            }
+       
+        outbox.pop(); // then we remove from the outbox stack 
+    }
+    
+   
 
    // should return the number of items in the queue
-   this.size = function(){
-     // TODO: implement `size`
+   this.size = function(){// suppose to be the sum of the two stacks 
+     return inbox.size() + outbox.size() // TODO: implement `size`
    };
  };
