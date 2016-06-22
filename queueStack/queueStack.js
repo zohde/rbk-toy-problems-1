865be1544879031f0757b09c67b0cf9677919441
Hadeel <hadylgk87@gmail.com>
2016-06-22 11:55:29 +0300

    DONE

--- queueStack/queueStack.js
+++ queueStack/queueStack.js
@@ -7,17 +7,20 @@
    * Stack Class
    */
  var Stack = function() {
-
+    this.storage = [];
    // add an item to the top of the stack
-   this.push = function(){
+   this.push = function(val){
+    this.storage.push(val);
    };
 
    // remove an item from the top of the stack
    this.pop = function(){
+    return this.storage.shift();
    };
 
    // return the number of items in the stack
    this.size = function(){
+    return this.storage.length;
    };
  };
 
@@ -30,17 +33,27 @@
    var outbox = new Stack();
 
    // called to add an item to the `queue`
-   this.enqueue = function(){
-     // TODO: implement `enqueue`
+   this.enqueue = function(value){
+     inbox.push(value);
+     
+     return true;
    };
 
    // called to remove an item from the `queue`
    this.dequeue = function(){
-     // TODO: implement `dequeue`
+     if(inbox.size() === 0)
+      return "queue is empty";
+    else{
+
+      outbox.push(inbox.pop());
+      var temp = outbox.pop();
+      return temp;
+    }
+    
    };
 
    // should return the number of items in the queue
    this.size = function(){
-     // TODO: implement `size`
+     return inbox.size();
    };
  };
