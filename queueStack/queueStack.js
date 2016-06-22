e015f670ac04ba4f2fddf2cf483f1119933f090a
WeaamShahwan <shahwamweaam@gmail.com>
2016-06-22 11:55:09 +0300

    solve queueStack toy problem

--- queueStack/queueStack.js
+++ queueStack/queueStack.js
@@ -7,17 +7,20 @@
    * Stack Class
    */
  var Stack = function() {
-
+  var str=[];
    // add an item to the top of the stack
    this.push = function(){
+  str.push.apply(str,arguments);
    };
 
    // remove an item from the top of the stack
    this.pop = function(){
+    return str.pop.apply(str, arguments);
    };
 
    // return the number of items in the stack
    this.size = function(){
+    return str.length;
    };
  };
 
@@ -32,15 +35,26 @@
    // called to add an item to the `queue`
    this.enqueue = function(){
      // TODO: implement `enqueue`
+     inbox.push.apply(inbox, arguments);
    };
 
    // called to remove an item from the `queue`
    this.dequeue = function(){
+
+
+    if (outbox.size()===0){
+
+      }else if(outbox.push()!==inbox.pop()){
+
+}
+
+return outbox.pop();
      // TODO: implement `dequeue`
    };
 
    // should return the number of items in the queue
    this.size = function(){
      // TODO: implement `size`
+     return inbox.size()
    };
  };
