014fb8250ac3e44255495ac2fc356b06b37729f8
maryamelham91 <maryamelham7@gmail.com>
2016-06-22 12:00:22 +0300

    queue stack

--- queueStack/queueStack.js
+++ queueStack/queueStack.js
@@ -7,19 +7,39 @@
    * Stack Class
    */
  var Stack = function() {
+ var store={};
+ var key=0;
 
    // add an item to the top of the stack
-   this.push = function(){
+   this.push = function(val){
+    this.store[key]=val;
+    this.key++;
+
    };
 
    // remove an item from the top of the stack
    this.pop = function(){
+  var value=store[key-1]; 
+    if(key!==0){
+    delete store[key-1];
+    key--;
+      }
+    else{
+      return 0;
+    }
+    return value;
+  
+ };
    };
 
    // return the number of items in the stack
    this.size = function(){
-   };
+    return this.key;
+  
  };
+
+//the main idea is to divide the queue to the 2 stacks so i have to add elem to the queue and after that
+//push it to the stack inbox after by enquqe and dequeue it from the outbox stack  
 
  /**
    * Queue Class
@@ -28,19 +48,39 @@
    // Use two `stack` instances to implement your `queue` Class
    var inbox = new Stack();
    var outbox = new Stack();
-
+   var queueS={};
+   var start=0;
+   var end=0;
    // called to add an item to the `queue`
-   this.enqueue = function(){
-     // TODO: implement `enqueue`
-   };
+   this.enqueue = function(value){//should add first elem in queue to the inbox stack
+    this.queueS[start]= value;
+      this.inbox[key]=queueS[start];
+    start++;
+    key++;
+    };
 
    // called to remove an item from the `queue`
-   this.dequeue = function(){
-     // TODO: implement `dequeue`
+   this.dequeue = function(){//should remove first elem in the outbox
+    this.outbox[key]=inbox[key]
+    key++;
+
+     //delete inbox[key]
+     delete outbox[key];
+      end++;
+      key--;
+    }
+    return outbox
    };
 
    // should return the number of items in the queue
    this.size = function(){
-     // TODO: implement `size`
-   };
+    if(start>end){
+      var d=start-end
+      return d;
+    }
+  
+    return 0;
+  
+   
  };
+
