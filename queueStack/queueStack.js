1e89bb7160463aa70a615a7b2f488f3421593a9e
AwsAhmed <aws.y.ahmed@gmail.com>
2016-06-22 11:56:23 +0300

    tryig

--- queueStack/queueStack.js
+++ queueStack/queueStack.js
@@ -7,19 +7,27 @@
    * Stack Class
    */
  var Stack = function() {
-
+   this.storage = [];
    // add an item to the top of the stack
-   this.push = function(){
+   this.push = function(item){
+    this.storage.push(item);
    };
 
    // remove an item from the top of the stack
    this.pop = function(){
+    this.storage.pop();
    };
 
    // return the number of items in the stack
    this.size = function(){
+    return this.storage.length;
    };
  };
+
+
+// Stack last in first out (LIFO)
+// Queue first in last out (FIFO)
+ 
 
  /**
    * Queue Class
@@ -29,18 +37,25 @@
    var inbox = new Stack();
    var outbox = new Stack();
 
+
+
    // called to add an item to the `queue`
-   this.enqueue = function(){
+   this.enqueue = function(item){
      // TODO: implement `enqueue`
+     outbox.push(item);
+     inbox.push(outbox[0]);
    };
 
    // called to remove an item from the `queue`
    this.dequeue = function(){
      // TODO: implement `dequeue`
+     var out = outbox.push()
+     inbox.push(outbox[0]);
    };
 
    // should return the number of items in the queue
    this.size = function(){
      // TODO: implement `size`
+     return inbox.size();
    };
  };
