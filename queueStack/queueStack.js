b9b47940637dd2c2372235a112edc1db8772ad87
Lamiaa Al Olabi <Lamiaa.olabi@gmail.com>
2016-06-22 11:58:49 +0300

    Stackqueue toy problem.

--- queueStack/queueStack.js
+++ queueStack/queueStack.js
@@ -7,19 +7,32 @@
    * Stack Class
    */
  var Stack = function() {
+  Stack.prototype= Object.create(stackMethods);
+  this.storage = {};
+  this.counter= 0;
+ };
 
+ var stackMethods = {};
    // add an item to the top of the stack
-   this.push = function(){
-   };
+  stackMethods.push= function(item){
+    this.storage[this.counter] = value;
+    this.counter++;
+
+   },
 
    // remove an item from the top of the stack
-   this.pop = function(){
-   };
+   stackMethods.pop = function(){
+    var pOp = this.storage[this.counter - 1];
+    delete this.storage[this.counter - 1];
+    this.counter--;
+    return pOp;
+   },
 
    // return the number of items in the stack
-   this.size = function(){
-   };
- };
+   stackMethods.size = function(){
+    var size = Object.keys(this.storage).lenght;
+    return size;
+   }
 
  /**
    * Queue Class
@@ -30,17 +43,20 @@
    var outbox = new Stack();
 
    // called to add an item to the `queue`
-   this.enqueue = function(){
+   this.enqueue = function(item){
+      inbox.push(item);
      // TODO: implement `enqueue`
    };
 
    // called to remove an item from the `queue`
    this.dequeue = function(){
-     // TODO: implement `dequeue`
-   };
+    // maybe call the enqueue method again to put the items inside the 'outbox';
+    // after that use the pop method: outbox.pop();
+       };
 
    // should return the number of items in the queue
    this.size = function(){
+      inbox.size();
      // TODO: implement `size`
    };
  };
