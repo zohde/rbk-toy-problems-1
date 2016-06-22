7fd4c116c8a5070b7ee3d0d86daf43aeb1952083
Bader Khalifeh <bader.a.khalifeh@gmail.com>
2016-06-22 11:45:47 +0300

    Implemented the Queue/Stack Solution

--- queueStack/queueStack.js
+++ queueStack/queueStack.js
@@ -8,16 +8,21 @@
    */
  var Stack = function() {
 
+ 	this.storage = [];
+
    // add an item to the top of the stack
-   this.push = function(){
+   this.push = function( value ){
+   	this.storage.push(value);
    };
 
    // remove an item from the top of the stack
    this.pop = function(){
+   	return this.storage.pop();
    };
 
    // return the number of items in the stack
    this.size = function(){
+   	return this.storage.length;
    };
  };
 
@@ -30,17 +35,28 @@
    var outbox = new Stack();
 
    // called to add an item to the `queue`
-   this.enqueue = function(){
+   this.enqueue = function(value){
      // TODO: implement `enqueue`
+     inbox.push(value);
    };
 
    // called to remove an item from the `queue`
    this.dequeue = function(){
      // TODO: implement `dequeue`
+     var iterationCount = inbox.size();
+     for(var i=0; i<iterationCount; i++){
+     	outbox.push(inbox.pop());
+     }
+     var result = outbox.pop();
+     for(var i=0; i<iterationCount-1; i++){
+     	inbox.push(outbox.pop());
+     }
+  	 return result;
    };
 
    // should return the number of items in the queue
    this.size = function(){
      // TODO: implement `size`
+     return inbox.size();
    };
  };
