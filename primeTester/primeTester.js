bbb2f7e6dc3f9967734911437fc9e0d55111715b
Saif Elokour <saiffelokour@gmail.com>
2016-06-26 11:45:18 +0300

    I Love Prime Numbers

--- primeTester/primeTester.js
+++ primeTester/primeTester.js
@@ -5,7 +5,13 @@
  */
 
 var primeTester = function(n) {
-  // to do
+  
+  for(var i = 2; i < n; i++){
+  	if(n % i === 0){
+  		return false
+  	}
+  }
+  return true;
 };
 
  /* Extra credit: Write a function that generates a list of all prime numbers
@@ -16,5 +22,49 @@ var primeTester = function(n) {
 
 
 var primeSieve = function(start, end) {
-  // to do
+  var array = []
+  var bool;
+  for(var i = start; i <= end; i++){
+  	  if(i === 1){
+  	  	array.push(1)
+  	  	array.push(2)
+  	  	i = i + 2
+  	  }
+  	  else if(i === 2){
+  	  	array.push(2)
+  	  	i++
+  	  }
+	  bool = true
+	  for(var j = 2; j < i; j++){
+	  	if(i % j === 0){
+	  		bool = false
+	  	}
+	  }
+	  if(bool){
+	  	array.push(i)
+	  }
+  }
+  return array
 };
+
+var primeSieveEra = function(start, end) {
+	var list = []
+	for(var i = 2; i <= end; i++){
+		list.push(true)
+	}
+	for(var j = 2; j <= Math.sqrt(end); j++){
+		if(list[j]){
+			for(var k = j*j; k <= end; k+=j){
+				list[k] = false
+			}
+		}
+	}
+	var array = []
+	for(var n = start; n <= end; n++){
+		if(list[n]){
+			array.push(n)
+		}
+	}
+	return array
+}
+
--- primeTester/spec/primeTester.js
+++ primeTester/spec/primeTester.js
@@ -1,5 +1,19 @@
 describe('primeTester', function() {
-  it('should return the right answer', function() {
-    expect(1).to.equal(1);
+  it('should return true if the number is prime', function() {
+    expect(primeTester(5)).to.equal(true);
+    expect(primeTester(13)).to.equal(true);
+    expect(primeTester(29)).to.equal(true);
   });
+  it('should return false if the number is not prime', function() {
+    expect(primeTester(6)).to.equal(false);
+    expect(primeTester(14)).to.equal(false);
+    expect(primeTester(28)).to.equal(false);
+  });  
+describe('primeSieve', function() {
+  it('should return of all prime numbers in a chosen range', function() {
+    expect(primeSieve(2,10)).to.equal([2, 3, 5, 7]);
+    expect(primeSieve(20,30)).to.equal([23,29]);
+    expect(primeSieve(5,25)).to.equal([5,7,11,13,17,19,23]);
+  });
+})
 });
