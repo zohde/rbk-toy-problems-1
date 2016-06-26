452072ccd7eaa52c168723ef3b8ae9eda02563c1
mihyar-30614 <eng.mihyear@gmail.com>
2016-06-26 11:43:15 +0300

    done with two different ways

--- primeTester/primeTester.js
+++ primeTester/primeTester.js
@@ -6,6 +6,28 @@
 
 var primeTester = function(n) {
   // to do
+  re=true;
+if(n === 2)
+	return true
+	else{
+	  for (var i = 2; i <n; i++) {
+	  	if(n%i === 0)
+	  		re= false
+	  }
+  }
+  return re;
+};
+
+
+//Another solution 
+var primeTester2 = function(n) {
+	if(n ===2 || n===3 || n=== 5 || n===7)
+		return true;
+	else {
+		if(n%2===0 || n%3 === 0 || n%5 ===0 || n%7 ===0)
+			return false
+	}
+	return true;
 };
 
  /* Extra credit: Write a function that generates a list of all prime numbers
@@ -16,5 +38,10 @@ var primeTester = function(n) {
 
 
 var primeSieve = function(start, end) {
-  // to do
+	var results = [];
+  for (var i = start; i <= end; i++) {
+  	if(primeTester2(i))
+  		results.push(i)
+  }
+  return results;
 };
