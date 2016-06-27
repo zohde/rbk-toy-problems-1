549e0662741aa01ac7e7cba0ca2aad1db47f60be
Fatima Hammami <en.f.h@live.com>
2016-06-27 11:59:05 +0300

    allanagrams

--- allAnagrams/allAnagrams.js
+++ allAnagrams/allAnagrams.js
@@ -15,6 +15,35 @@
   * console.log(anagrams); // [ 'abc', 'acb', 'bac', 'bca', 'cab', 'cba' ]
   */
 
-var allAnagrams = function(string) {
+var factorial = function(n) {
+    if(n == 0) {
+        return 1
+    } else {
+        return n * factorial(n - 1);
+    }
+}
 
+
+var allAnagrams = function(string) {
+	var str="";
+	var results=[];
+	var randomAr=[];
+	var fac=factorial(string.length);
+	for (var i = 0; i < string.length; i++) {
+		var random=Math.floor(Math.random()*i);
+		if(randomAr.indexOf(random) === -1){
+			randomAr.push(random);
+			str+=string[random];
+		}
+	}
+	if(results.indexOf(str) === -1){
+		results.push(str);
+		fac--;
+	}
+
+	if(results.length !== fac){
+		allAnagrams(string);
+	}else{
+		return results;
+	}
 };
