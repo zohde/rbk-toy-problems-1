f8b8f355e5dd62cf795bd9736661db5bb4a3811a
Bader Khalifeh <bader.a.khalifeh@gmail.com>
2016-06-27 11:54:29 +0300

    Implemented AllAngrams Solution

--- allAnagrams/allAnagrams.js
+++ allAnagrams/allAnagrams.js
@@ -15,6 +15,42 @@
   * console.log(anagrams); // [ 'abc', 'acb', 'bac', 'bca', 'cab', 'cba' ]
   */
 
-var allAnagrams = function(string) {
 
+var indexOfFunc= function(arr, val){
+	for(var i=0; i<arr.length; i++){
+		if(arr[i] === val){
+			return true;
+		}
+	}
+	return false;
+}
+
+var pushIntoArr = function(arr, val){
+	var array = [];
+	for(var i=0; i<arr.length; i++){
+		array.push(arr[i]);
+	}
+	array.push(val);
+	return array;
+}
+
+var allAnagrams = function(string) {
+	var resultArr =[];
+
+	var recursiveFunc = function(accumStr, count, indexArr){
+
+		if(count === string.length){
+			resultArr.push(accumStr);
+		}
+
+		for(var i=0; i<string.length; i++){
+			if(!indexOfFunc(indexArr, i)){
+				recursiveFunc(accumStr + string[i], count+1, pushIntoArr(indexArr,i))
+			}
+		}
+	}
+
+	recursiveFunc('',0, []);
+
+	return resultArr;
 };
