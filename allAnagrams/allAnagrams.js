93820a15cce426b1702ef2f313aaf4d7ca65009f
emano93 <emanothman506@gmail.com>
2016-06-27 11:57:52 +0300

    :(

--- allAnagrams/allAnagrams.js
+++ allAnagrams/allAnagrams.js
@@ -16,5 +16,26 @@
   */
 
 var allAnagrams = function(string) {
+	string=string.split("");
+	result=[];
+	for(var i=0;i<string.length;i++){
+		for (var h = 0; h < string.length; h++) {
+			for (var j = 0; j < string.length.length; j++) {
+			result.push(string[i],string[h],string[j]);
+			}
+			
+		}
+	}
+	return result;
 
+};
+var allAnagrams = function(string) {
+	var result=[];
+	function implement(element){
+		for(var i=0;i<string.length;i++){
+			result.push(string[i])+implement(string[i+1]);
+		}
+	}
+   implement(string)
+	return result;
 };
