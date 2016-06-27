4656093856f86e6d995fa4046b102cace8f1ca40
Lamiaa Al Olabi <Lamiaa.olabi@gmail.com>
2016-06-27 11:58:14 +0300

    All Anagrams, the hardest one yet!

--- allAnagrams/allAnagrams.js
+++ allAnagrams/allAnagrams.js
@@ -16,5 +16,28 @@
   */
 
 var allAnagrams = function(string) {
+// input = "string";
+// output = ['','',''];
+// check for edge cases like if the input wasn't a string;
+// declare an array to store the output;
+	var arr = [];
+	if (typeof(string) !== 'string'){
+		return undefined;
+	} else {
+		// loop over the whole string length;
+		for (var i = 0; i < string.length; i++) {
+		// declare a variable that is a function which recurse over the remaining length of the string;
+		var str = function('string'){
+			// declaring a str2 to store the results of the recursive function.
+			var str2= '';
+			//idealy this function should take the first letter(i.e. index) of the string, 
+			// recurse over the remaining letters until it reaches the end of the length.
+			// return str2; 
+		}
 
+		}
+		// outside the main loop will push str2 into arr
+	}
+	// finally return arr;
+	return arr;
 };
