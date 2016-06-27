37e3874b98790d5ba6766630308d75e65abd3d62
farahamawi <amawifarah@gmail.com>
2016-06-27 12:59:31 +0400

    allAnagrams

--- allAnagrams/allAnagrams.js
+++ allAnagrams/allAnagrams.js
@@ -14,7 +14,22 @@
   * var anagrams = allAnagrams('abc');
   * console.log(anagrams); // [ 'abc', 'acb', 'bac', 'bca', 'cab', 'cba' ]
   */
+var factorial = function(string){
+	var result = 1;
+	for (var i = 1; i < string.length; i++) {
+		result=result*i;
+	}
+	return result;
+}
+
 
 var allAnagrams = function(string) {
+	var arr = [];
+	if(arr.length = factorial(string)){
+		return arr;
+	}
+	var str =""
+	//1st element has string.length options and 
+	//2nd element has string.length-1 options and .... etc
 
 };
