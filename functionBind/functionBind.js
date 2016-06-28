f8ae3529b3adfb1c2518c5b2422d4679efedc50e
Salah Alomari <s.z.alomari.1987@gmail.com>
2016-06-28 12:19:35 +0400

    Write tests, function prototype.bind

--- functionBind/functionBind.js
+++ functionBind/functionBind.js
@@ -1,57 +1,23 @@
 
- // function bind():
- //  example 1:
- //  var alice = {
- //   name: 'alice',
- //   shout: function(){
- //     alert(this.name);
- //   }
- // }
- // var boundShout = bind(alice.shout, alice);
- // boundShout(); // alerts 'alice'
- // boundShout = bind(alice.shout, {name: 'bob'});
- // boundShout(); // alerts 'bob'
- // //  example 2:
- //  var func = function(a, b){ return a + b };
- // var boundFunc = bind(func, null, 'foo');
- // var result = boundFunc('bar');
- // result === 'foobar'; // true
-
-
 var bind = function(func, context, ...args) {
-
 	return function (){
-		for (var key in arguments){
-			args[args.length+parseFloat(key)] = arguments[parseFloat(key)	]
-		}
-		return func.apply(context, args);
+		arg = args.concat([].slice.call(arguments));
+		return func.apply(context, arg);
 	}	
 };
 
- // * Function.prototype.bind:
- // *
- // * example 1:
- // *
- // * var alice = {
- // *   name: 'alice',
- // *   shout: function(){
- // *     alert(this.name);
- // *   }
- // * }
- // * var boundShout = alice.shout.bind(alice);
- // * boundShout(); // alerts 'alice'
- // * boundShout = alice.shout.bind({name: 'bob'});
- // * boundShout(); // alerts 'bob'
- // *
- // * example 2:
- // *
- // * var func = function(a, b){ return a + b };
- // * var boundFunc = func.bind(null, 'foo');
- // * var result = boundFunc('bar');
- // * result === 'foobar'; // true
- // *
-
-
 Function.prototype.bind = function(a, args1, args2) {
-	
+	var that = this;
+	var args = [];
+	args.push(args1);
+	args.push(args2);
+	return function (){
+		arg = [].slice.call(arguments)
+		for (var i=0; i<args.length; i++){
+			if (args[i]){
+				arg.unshift(args[i])
+			}
+		}
+		return that.apply(a,arg);
+	}	
 };
--- functionBind/spec/functionBind.js
+++ functionBind/spec/functionBind.js
@@ -1,5 +1,55 @@
 describe('functionBind', function() {
-  it('should return the right answer', function() {
-    expect(1).to.equal(1);
+  var func = bind()
+  it('should be a function', function() {
+    expect(func).to.be.a('function');
   });
+});
+describe('functionBind', function() {
+   var alice = {
+	   name: 'alice',
+	   shout: function(){
+	     return this.name;
+	   }
+	 }
+ 	var boundShout = bind(alice.shout, alice);
+ 	var result = boundShout()
+  	it('should return alice', function() {
+    	expect(result).to.equal('alice');
+ 	});
+  	boundShout2 = bind(alice.shout, {name: 'bob'});
+ 	var result2 = boundShout2();
+  	it('should return bob', function() {
+  	  expect(result2).to.equal('bob');
+  	});
+  	 var func = function(a, b){ return a + b };
+	 var boundFunc = bind(func, null, 'foo');
+	 var result3 = boundFunc('bar');
+	it('should return foobar', function() {
+  	  expect(result3).to.equal('foobar');
+  	});
+});
+
+describe('Function.prototype.bind', function() {
+   var alice = {
+	   name: 'alice',
+	   shout: function(){
+	     return this.name;
+	   }
+	 }
+ 	var boundShout = alice.shout.bind(alice);
+ 	var result = boundShout()
+  	it('should return alice', function() {
+    	expect(result).to.equal('alice');
+ 	});
+  	boundShout2 = alice.shout.bind({name: 'bob'});
+ 	var result2 = boundShout2();
+  	it('should return bob', function() {
+  	  expect(result2).to.equal('bob');
+  	});
+  	var func = function(a, b){ return a + b };
+  	var boundFunc = func.bind(null, 'foo');
+  	var result3 = boundFunc('bar');
+  	it('should return foobar', function() {
+  	  expect(result3).to.equal('foobar');
+  	});
 });
