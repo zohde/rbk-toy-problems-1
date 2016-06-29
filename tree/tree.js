5f8080f06c83e2d3ed7851eabbfc2f4f823c8335
Admin <admin@NetBoot-8.local>
2016-06-29 12:11:11 +0400

    Implement tree

--- tree/tree.js
+++ tree/tree.js
@@ -3,20 +3,42 @@ Your tree should have methods named "addChild" and "contains".
 */
 
 // EXAMPLE USAGE:
-// var tree = treeMaker();
+// var tree = treeMaker(5);
 // tree.addChild(1);
 // tree.addChild(2);
 // tree.contains(2);   // yields 'true'
 
 var treeMaker = function(value){
- //tree code goes here!
+	var instance = Object.create(methods);
+	instance.value = value;
+	instance.children = [];
+	return instance;
 };
 
 //methods go here!
-treeMaker.methods = {};
-
-treeMaker.methods.addChild = function(){
+var methods = {
+		addChild: function (value) {
+			this.children.push(treeMaker(value));
+		},
+		contains: function (value) {
+			if (this.value === value) {
+        		return true;
+      		} else if(this.children.length > 0) {
+          		for (var i = 0; i < this.children.length; i++) {
+          			if(this.children[i].value === value){
+          				return true;
+          			}
+          		}
+          		for (var i = 0; i < this.children.length; i++) {
+              		this.children[i].contains(value);
+            	}
+      		}
+      		return false;
+		}
 };
 
-treeMaker.methods.contains = function(){
-};
+// treeMaker.methods.addChild = function(){
+// };
+
+// treeMaker.methods.contains = function(){
+// };
