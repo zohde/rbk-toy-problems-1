a8eb5ce2d0ed4369b46572859d780f29305309b5
mihyar-30614 <eng.mihyear@gmail.com>
2016-06-29 12:03:48 +0300

    working for the first set of children

--- tree/tree.js
+++ tree/tree.js
@@ -10,13 +10,23 @@ Your tree should have methods named "addChild" and "contains".
 
 var treeMaker = function(value){
  //tree code goes here!
+ var Root = Object.create(treeMaker.methods);
+ Root.value=value;
+ Root.children=[];
+return Root;
 };
 
 //methods go here!
 treeMaker.methods = {};
 
-treeMaker.methods.addChild = function(){
+treeMaker.methods.addChild = function(value){
+	this.children.push(new treeMaker(value))
 };
 
-treeMaker.methods.contains = function(){
+treeMaker.methods.contains = function(find){
+	for (var i=0; i<this.children.length; i++){
+		if(this.children[i].value === find)
+			return true;
+		return false;
+	}
 };
