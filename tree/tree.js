0af99d9551fb3a63227a79ebd7858507b5e15e07
Salah Alomari <s.z.alomari.1987@gmail.com>
2016-06-29 12:16:27 +0400

    Implement prototypal instantiation

--- tree/tree.js
+++ tree/tree.js
@@ -9,23 +9,23 @@ Your tree should have methods named "addChild" and "contains".
 // tree.contains(2);   // yields 'true'
 
 var treeMaker = function(value){
-	
-	treeMaker._storage = {};
-	treeMaker._storage.value = value?value:undefined;
-	treeMaker._storage.children = [];
-	return treeMaker.prototype;
+	var treeInst = Object.create(treeMaker.prototype)
+	treeInst._storage = {};
+	treeInst._storage.value = value?value:undefined;
+	treeInst._storage.children = [];
+	return treeInst;
 };
 
 treeMaker.prototype.addChild = function(value){
 	if(!this.contains(value)){
-		treeMaker._storage.children.push(value)
+		this._storage.children.push(value)
 	}
 };
 
 treeMaker.prototype.contains = function(value){
-	if (treeMaker._storage.value === value){
+	if (this._storage.value === value){
 		return true;
-	} else if (treeMaker._storage.children.indexOf(value)!==-1){
+	} else if (this._storage.children.indexOf(value)!==-1){
 		return true;
 	};
 	return false
