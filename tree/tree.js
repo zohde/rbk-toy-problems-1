bccd26d0c0fd3ff3ae6a2f5eb55f5aee5130f696
eshraq1991 <bakri.eshraq@gmail.com>
2016-06-29 12:16:41 +0300

    tree data structure

--- tree/tree.js
+++ tree/tree.js
@@ -10,13 +10,40 @@ Your tree should have methods named "addChild" and "contains".
 
 var treeMaker = function(value){
  //tree code goes here!
+	var storage=Object.create(treeMaker.methods);
+	storage.value=value;
+	storage.children=[];
+	return storage;
 };
 
 //methods go here!
 treeMaker.methods = {};
 
-treeMaker.methods.addChild = function(){
+treeMaker.methods.addChild = function(value){
+	var newChild= treeMaker(value);
+	this.children.push(newChild);
 };
 
-treeMaker.methods.contains = function(){
+treeMaker.methods.contains = function(value){
+	var result=false;
+	contain=this;
+	while(true){
+		if(contain.value!==null){
+		if(contain.value===value){
+			result=true;
+			break;
+		}else{
+			for(var i=0;i<contain.children.length;i++){
+				if(contain.children[i].value===value){
+					return true;
+				}else{
+					contain=children[i];
+				}
+			}
+		}
+	}else{
+		break;
+	}
+}
+	return result;
 };
