f98ccad03564f674c9683b611b80f9dd81ec69c7
Salah Alomari <s.z.alomari.1987@gmail.com>
2016-07-02 11:09:24 +0400

    Implement tree

--- tree/tree.js
+++ tree/tree.js
@@ -8,21 +8,19 @@ Your tree should have methods named "addChild" and "contains".
 // tree.addChild(2);
 // tree.contains(2);   // yields 'true'
 
-var treeMaker = function(value){
-	var treeInst = Object.create(treeMaker.prototype)
-	treeInst._storage = {};
-	treeInst._storage.value = value?value:undefined;
-	treeInst._storage.children = [];
-	return treeInst;
+var Tree = function(value){
+	this._storage = {};
+	this._storage.value = value?value:undefined;
+	this._storage.children = [];
 };
 
-treeMaker.prototype.addChild = function(value){
+Tree.prototype.addChild = function(value){
 	if(!this.contains(value)){
 		this._storage.children.push(value)
 	}
 };
 
-treeMaker.prototype.contains = function(value){
+Tree.prototype.contains = function(value){
 	if (this._storage.value === value){
 		return true;
 	} else if (this._storage.children.indexOf(value)!==-1){
