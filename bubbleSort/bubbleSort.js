4cc240b20df5026a84449db40a2c1446eec4b036
RaghadAlkhawaldeh <raghadkhawaldeh@gmail.com>
2016-07-10 08:59:56 +0300

    bubble

--- bubbleSort/bubbleSort.js
+++ bubbleSort/bubbleSort.js
@@ -30,5 +30,25 @@
 // Feel free to add helper functions if needed
 
 var bubbleSort = function(array) {
-
+	var arr=[];
+	for (var i = 0; i < array.length; i++) {
+		for (var j = 0; j < array.length-1; j++) {
+			if(array[j]>array[j+1]){
+		// 	arr.push(array[j+1]);
+		// 	//array.splice(0,1);
+		// }else{
+			var x=array[j];
+			array[j]=array[j+1];
+			array[j+1]=x;
+			}
+		}
+	}
+	return array;
 };
+
+
+//the time complexity of this function is O(N^2) where n is the length if the array.
+
+//the time complexity would be the same if the array was sorted since every two adjacent elements would be
+//checked.
+
