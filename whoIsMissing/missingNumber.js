f45d3965a6eb708f448c0ea14102fe45e9a7e181
ibrahimtamimi <ibrahimtamimi5@gmail.com>
2016-07-17 08:16:12 +0300

    who is missing

--- whoIsMissing/missingNumber.js
+++ whoIsMissing/missingNumber.js
@@ -9,13 +9,19 @@
 // EXAMPLE: When inputArray is [2,3,4,6], output should be 5
 
 var orderedArrayMissingNumber = function(orderedInputArray) {
-	// Your code here..
+	for (var i = 0; i < orderedInputArray.length-1 ; i++) {
+		if(orderedInputArray[i+1] - orderedInputArray[i] > 1){
+			console.log( orderedInputArray[i]+1 );
+		}
+	}
 }
-
+//O(n)
 // Input will not be in order.
 // EXAMPLE: When inputArray is [6,5,7,3,2], output should be 4.
 
 // What is the time complexity of your solution?
+//O(n)
 var unorderedArrayMissingNumber = function(unorderedInputArray) {
-	// Your code here..
+	var orderedInputArray=unorderedInputArray.sort();
+	orderedArrayMissingNumber(orderedInputArray);
 }
