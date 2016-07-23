371c923c1ce92e27e15cb257ab8184abb12d6bf5
ibrahimtamimi <ibrahimtamimi5@gmail.com>
2016-07-23 09:12:47 +0300

    fix sorting

--- largestProductOfThree/largestProductOfThree.js
+++ largestProductOfThree/largestProductOfThree.js
@@ -7,7 +7,20 @@
  * Extra credit: Make your function handle negative numbers.
  */
 
+function sort(arrayX){
+	for (i = 0; i <= arrayX.length-1; i++){
+    	if (arrayX[i] > arrayX[i+1]){
+        	temp = arrayX[i+1];
+        	arrayX[i+1] = arrayX[i];
+        	arrayX[i] = temp;
+        	i = i-2;
+      	}
+  	}
+  	return arrayX
+}
+
 var largestProductOfThree = function(array) {
-	var products=array.sort();
+	var products=sort(array)
+	console.log(products)
 	return products[products.length-1] * products[products.length-2] * products[products.length-3]
 };
