e1b80ec5ea78e9d0f609dcdfba37c205a72a5586
ibrahimtamimi <ibrahimtamimi5@gmail.com>
2016-07-27 09:56:28 +0300

    combination

--- coinSum/coinSum.js
+++ coinSum/coinSum.js
@@ -14,6 +14,46 @@ coinCombination(200p) //-> 73682
 */
 
 var coinCombination = function(totalAmount){
-	var totalNumberOfCombinations = 0;
+	var totalNumberOfCombinations  = 0
+ 
+	for (var a = totalAmount; a >= 0; a -= 200) {
+	    for (var b = a; b >= 0; b -= 100) {
+	        for (var c = b; c >= 0; c -= 50) {
+	            for (var d = c; d >= 0; d -= 20) {
+	                for (var e = d; e >= 0; e -= 10) {
+	                    for (var f = e; f >= 0; f -= 5) {
+	                        for (var g = f; g >= 0; g -= 2) {
+	                            totalNumberOfCombinations++;
+	                        }
+	                    }
+	                }
+	            }
+	        }
+	    }
+	}
 	return totalNumberOfCombinations
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
