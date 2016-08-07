ba538cc9a22403e6a2f5f93d7735387c1e95012d
ibrahimtamimi <ibrahimtamimi5@gmail.com>
2016-08-07 09:30:43 +0300

    majorityElement

--- majorityElement/majorityElement.js
+++ majorityElement/majorityElement.js
@@ -9,5 +9,20 @@ findAll([17,4,12,-16,12,4,7,12]) //should return [12]
 */
 
 var findAll = function(arr){
+	var coll={};
+	var res=[];
+	for (var i = 0; i < arr.length; i++) {
+		 if(coll[arr[i]] === undefined) {
+		 	coll[arr[i]] = 0;
+		 }
+		 coll[arr[i]]++;
+	}
 
+	for(var k in coll){
+		if (coll[k] % 3 === 0 ){
+			res.push(parseInt(k))
+		}
+	}
+	console.log(coll)
+	return res;
 }
