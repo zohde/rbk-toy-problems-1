c4dc89ca71fee0e3dfa2e7c53c3ed0fef09a7e90
pavelmp <pavelmp@gmail.com>
2016-08-26 21:26:31 +0300

    Add longestPalindrome

---
+++ longestPalindrome/longestPalindrome.js
@@ -0,0 +1,16 @@
+/**
+* Implement a function that finds the longest palindrome in a given string.
+* Palindrome is a string that is the same when you read it from right to left
+* and from left to right.
+* For example, in the string "My dad is a racecar athlete", the longest
+* palindrome is "a racecar a". Count whitespaces as valid characters. Other
+* palindromes in the above string include "dad", "ete", " dad " (including
+* whitespace on each side of dad).
+
+* Extra credit: The best solution for this problem runs in linear time.
+* If you would like a challenge, read about Manacher's algorithm
+*/
+
+var longestPalindrome = function (string) {
+  
+};
